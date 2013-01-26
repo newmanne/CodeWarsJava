25324e58c25d2df1a9eef5e4f1dd1892da1ac0e2
Neil <newman-n@hotmail.com>
2013-01-26 12:48:05 -0500

    Init commit

---
+++ src/net/windward/Windwardopolis/AI/MyPlayerBrain.java
@@ -0,0 +1,266 @@
+package net.windward.Windwardopolis.AI;
+
+
+// Created by Windward Studios, Inc. (www.windward.net). No copyright claimed - do anything you want with this code.
+
+
+import net.windward.Windwardopolis.api.Company;
+import net.windward.Windwardopolis.api.Map;
+import net.windward.Windwardopolis.api.Passenger;
+import net.windward.Windwardopolis.api.Player;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * The sample C# AI. Start with this project but write your own code as this is a very simplistic implementation of the AI.
+ */
+public class MyPlayerBrain implements net.windward.Windwardopolis.AI.IPlayerAI {
+    // bugbug - put your team name here.
+    private static String NAME = "James Gosling";
+
+    // bugbug - put your school name here. Must be 11 letters or less (ie use MIT, not Massachussets Institute of Technology).
+    public static String SCHOOL = "Windward U.";
+
+    /**
+     * The name of the player.
+     */
+    private String privateName;
+
+    public final String getName() {
+        return privateName;
+    }
+
+    private void setName(String value) {
+        privateName = value;
+    }
+
+    /**
+     * The game map.
+     */
+    private Map privateGameMap;
+
+    public final Map getGameMap() {
+        return privateGameMap;
+    }
+
+    private void setGameMap(Map value) {
+        privateGameMap = value;
+    }
+
+    /**
+     * All of the players, including myself.
+     */
+    private java.util.ArrayList<Player> privatePlayers;
+
+    public final java.util.ArrayList<Player> getPlayers() {
+        return privatePlayers;
+    }
+
+    private void setPlayers(java.util.ArrayList<Player> value) {
+        privatePlayers = value;
+    }
+
+    /**
+     * All of the companies.
+     */
+    private java.util.ArrayList<Company> privateCompanies;
+
+    public final java.util.ArrayList<Company> getCompanies() {
+        return privateCompanies;
+    }
+
+    private void setCompanies(java.util.ArrayList<Company> value) {
+        privateCompanies = value;
+    }
+
+    /**
+     * All of the passengers.
+     */
+    private java.util.ArrayList<Passenger> privatePassengers;
+
+    public final java.util.ArrayList<Passenger> getPassengers() {
+        return privatePassengers;
+    }
+
+    private void setPassengers(java.util.ArrayList<Passenger> value) {
+        privatePassengers = value;
+    }
+
+    /**
+     * Me (my player object).
+     */
+    private Player privateMe;
+
+    public final Player getMe() {
+        return privateMe;
+    }
+
+    private void setMe(Player value) {
+        privateMe = value;
+    }
+
+    private PlayerAIBase.PlayerOrdersEvent sendOrders;
+
+    private static final java.util.Random rand = new java.util.Random();
+
+    public MyPlayerBrain(String name) {
+        setName(!net.windward.Windwardopolis.DotNetToJavaStringHelper.isNullOrEmpty(name) ? name : NAME);
+    }
+
+    /**
+     * The avatar of the player. Must be 32 x 32.
+     */
+    public final byte[] getAvatar() {
+        try {
+            // open image
+            File file = new File(getClass().getResource("/net/windward/Windwardopolis/res/MyAvatar.png").getFile());
+
+            FileInputStream fisAvatar = new FileInputStream(file);
+            byte [] avatar = new byte[fisAvatar.available()];
+            fisAvatar.read(avatar, 0, avatar.length);
+            return avatar;
+
+        } catch (IOException e) {
+            System.out.println("error reading image");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Called at the start of the game.
+     *
+     * @param map         The game map.
+     * @param me          You. This is also in the players list.
+     * @param players     All players (including you).
+     * @param companies   The companies on the map.
+     * @param passengers  The passengers that need a lift.
+     * @param ordersEvent Method to call to send orders to the server.
+     */
+    public final void Setup(Map map, Player me, java.util.ArrayList<Player> players, java.util.ArrayList<Company> companies, java.util.ArrayList<Passenger> passengers, PlayerAIBase.PlayerOrdersEvent ordersEvent) {
+
+        try {
+            setGameMap(map);
+            setPlayers(players);
+            setMe(me);
+            setCompanies(companies);
+            setPassengers(passengers);
+            sendOrders = ordersEvent;
+
+            java.util.ArrayList<Passenger> pickup = AllPickups(me, passengers);
+
+            // get the path from where we are to the dest.
+            java.util.ArrayList<Point> path = CalculatePathPlus1(me, pickup.get(0).getLobby().getBusStop());
+            sendOrders.invoke("ready", path, pickup);
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Called to send an update message to this A.I. We do NOT have to send orders in response.
+     *
+     * @param status     The status message.
+     * @param plyrStatus The player this status is about. THIS MAY NOT BE YOU.
+     * @param players    The status of all players.
+     * @param passengers The status of all passengers.
+     */
+    public final void GameStatus(PlayerAIBase.STATUS status, Player plyrStatus, java.util.ArrayList<Player> players, java.util.ArrayList<Passenger> passengers) {
+
+        // bugbug - Framework.cs updates the object's in this object's Players, Passengers, and Companies lists. This works fine as long
+        // as this app is single threaded. However, if you create worker thread(s) or respond to multiple status messages simultaneously
+        // then you need to split these out and synchronize access to the saved list objects.
+
+        try {
+            // bugbug - we return if not us because the below code is only for when we need a new path or our limo hit a bus stop.
+            // if you want to act on other players arriving at bus stops, you need to remove this. But make sure you use Me, not
+            // plyrStatus for the Player you are updatiing (particularly to determine what tile to start your path from).
+            if (plyrStatus != getMe()) {
+                return;
+            }
+            Point ptDest = null;
+            java.util.ArrayList<Passenger> pickup = new java.util.ArrayList<Passenger>();
+            switch (status) {
+                case UPDATE:
+                    return;
+                case NO_PATH:
+                case PASSENGER_NO_ACTION:
+                    if (plyrStatus.getLimo().getPassenger() == null) {
+                        pickup = AllPickups(plyrStatus, passengers);
+                        ptDest = pickup.get(0).getLobby().getBusStop();
+                    } else {
+                        ptDest = plyrStatus.getLimo().getPassenger().getDestination().getBusStop();
+                    }
+                    break;
+                case PASSENGER_DELIVERED:
+                case PASSENGER_ABANDONED:
+                	System.out.println("Hi" + plyrStatus.getPassengersDelivered());
+                    pickup = AllPickups(plyrStatus, passengers);
+                    ptDest = pickup.get(0).getLobby().getBusStop();
+                    break;
+                case PASSENGER_REFUSED:
+                    //add in random so no refuse loop
+                    for (Company cpy : getCompanies()) {
+                        if (cpy != plyrStatus.getLimo().getPassenger().getDestination()) {
+                            ptDest = cpy.getBusStop();
+                            break;
+                        }
+                    }
+                    break;
+                case PASSENGER_DELIVERED_AND_PICKED_UP:
+                case PASSENGER_PICKED_UP:
+                    pickup = AllPickups(plyrStatus, passengers);
+                    ptDest = plyrStatus.getLimo().getPassenger().getDestination().getBusStop();
+                    break;
+                default:
+                    throw new RuntimeException("unknown status");
+            }
+
+            // get the path from where we are to the dest.
+            java.util.ArrayList<Point> path = CalculatePathPlus1(plyrStatus, ptDest);
+
+            // update our saved Player to match new settings
+            if (path.size() > 0) {
+                getMe().getLimo().getPath().clear();
+                getMe().getLimo().getPath().addAll(path);
+            }
+            if (pickup.size() > 0) {
+                getMe().getPickUp().clear();
+                getMe().getPickUp().addAll(pickup);
+            }
+
+            sendOrders.invoke("move", path, pickup);
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private java.util.ArrayList<Point> CalculatePathPlus1(Player me, Point ptDest) {
+        java.util.ArrayList<Point> path = SimpleAStar.CalculatePath(getGameMap(), me.getLimo().getMapPosition(), ptDest);
+        // add in leaving the bus stop so it has orders while we get the message saying it got there and are deciding what to do next.
+        if (path.size() > 1) {
+            path.add(path.get(path.size() - 2));
+        }
+        return path;
+    }
+
+    private static java.util.ArrayList<Passenger> AllPickups(Player me, Iterable<Passenger> passengers) {
+        java.util.ArrayList<Passenger> pickup = new java.util.ArrayList<Passenger>();
+
+        for (Passenger psngr : passengers) {
+            if ((!me.getPassengersDelivered().contains(psngr)) && (psngr != me.getLimo().getPassenger()) && (psngr.getCar() == null) && (psngr.getLobby() != null) && (psngr.getDestination() != null))
+                pickup.add(psngr);
+        }
+
+        //add sort by random so no loops for can't pickup
+        return pickup;
+    }
+}
---
+++ src/net/windward/Windwardopolis/AI/SimpleAStar.java
@@ -0,0 +1,306 @@
+package net.windward.Windwardopolis.AI;
+
+import java.awt.*;
+
+import net.windward.Windwardopolis.api.Map;
+import net.windward.Windwardopolis.api.MapSquare;
+import net.windward.Windwardopolis.TRAP;
+// No comments about how this is the world's worst A* implementation. It is purposely simplistic to leave the teams
+// the opportunity to improve greatly upon this. (I was yelled at last year for making the sample A.I.'s too good.)
+//
+// Also there is (at least) one very subtle bug in the below code. It is very rarely hit.
+
+
+/**
+ * The Pathfinder (maybe I should name it Fr�mont).
+ * Good intro at http://www.policyalmanac.org/games/aStarTutorial.htm
+ */
+public class SimpleAStar {
+    private static final Point[] offsets = {
+            new Point(-1, 0),
+            new Point(1, 0),
+            new Point(0, -1),
+            new Point(0, 1)
+    };
+
+    private static final int DEAD_END = 10000;
+
+    private static final Point ptOffMap = new Point(-1, -1);
+
+    /**
+     * Calculate a path from start to end. No comments about how this is the world's worst A* implementation. It is purposely
+     * simplistic to leave the teams the opportunity to improve greatly upon this. (I was yelled at last year for making the
+     * sample A.I.'s too good.)
+     *
+     * @param map   The game map.
+     * @param start The tile units of the start point (inclusive).
+     * @param end   The tile units of the end point (inclusive).
+     * @return The path from start to end.
+     */
+    public static java.util.ArrayList<Point> CalculatePath(Map map, Point start, Point end) {
+
+        // should never happen but just to be sure
+        if (start == end) {
+            return new java.util.ArrayList<Point>(java.util.Arrays.asList(new Point[]{start}));
+        }
+
+        // nodes are points we have walked to
+        java.util.HashMap<Point, TrailPoint> nodes = new java.util.HashMap<Point, TrailPoint>();
+        // points we have in a TrailPoint, but not yet evaluated.
+        java.util.ArrayList<TrailPoint> notEvaluated = new java.util.ArrayList<TrailPoint>();
+
+        TrailPoint tpOn = new TrailPoint(start, end, 0);
+        while (true) {
+            nodes.put(tpOn.getMapTile(), tpOn);
+
+            // get the neighbors
+            TrailPoint tpClosest = null;
+            for (Point ptOffset : offsets) {
+                Point pt = new Point(tpOn.getMapTile().x + ptOffset.x, tpOn.getMapTile().y + ptOffset.y);
+                MapSquare square = map.SquareOrDefault(pt);
+                // off the map or not a road/bus stop
+                if ((square == null) || (!square.getIsDriveable())) {
+                    continue;
+                }
+
+                // already evaluated - add it in
+                if (nodes.containsKey(pt)) {
+                    TrailPoint tpAlreadyEvaluated = nodes.get(pt);
+                    tpAlreadyEvaluated.setCost(Math.min(tpAlreadyEvaluated.getCost(), tpOn.getCost() + 1));
+                    tpOn.getNeighbors().add(tpAlreadyEvaluated);
+                    continue;
+                }
+
+                // add this one in
+                TrailPoint tpNeighbor = new TrailPoint(pt, end, tpOn.getCost() + 1);
+                tpOn.getNeighbors().add(tpNeighbor);
+
+                // may already be in notEvaluated. If so remove it as this is a more recent cost estimate
+
+                int indTp = -1;
+
+                for (int i = 0; i < notEvaluated.size(); i++) {
+                    TrailPoint tp = notEvaluated.get(i);
+                    if (tp.getMapTile() == tpNeighbor.getMapTile()) {
+                        indTp = i;
+                        break;
+                    }
+
+                }
+
+                if (indTp != -1)
+                    notEvaluated.remove(indTp);
+
+
+                // we only assign to tpClosest if it is closer to the destination. If it's further away, then we
+                // use notEvaluated below to find the one closest to the dest that we have not walked yet.
+                if (tpClosest == null) {
+                    if (tpNeighbor.getDistance() < tpOn.getDistance())
+                    // new neighbor is closer - work from this next.
+                    {
+                        tpClosest = tpNeighbor;
+                    } else
+                    // this is further away - put in the list to try if a better route is not found
+                    {
+                        notEvaluated.add(tpNeighbor);
+                    }
+                } else {
+                    if (tpClosest.getDistance() <= tpNeighbor.getDistance())
+                    // this is further away - put in the list to try if a better route is not found
+                    {
+                        notEvaluated.add(tpNeighbor);
+                    } else {
+                        // this is closer than tpOn and another neighbor - use it next.
+                        notEvaluated.add(tpClosest);
+                        tpClosest = tpNeighbor;
+                    }
+                }
+            }
+
+            // re-calc based on neighbors
+            tpOn.RecalculateDistance(ptOffMap, map.getWidth());
+
+            // if no closest, then get from notEvaluated. This is where it guarantees that we are getting the shortest
+            // route - we go in here if the above did not move a step closer. This may not either as the best choice
+            // may be the neighbor we didn't go with above - but we drop into this to find the closest based on what we know.
+            if (tpClosest == null) {
+                if (notEvaluated.isEmpty()) {
+                    TRAP.trap();
+                    break;
+                }
+                // We need the closest one as that's how we find the shortest path.
+                tpClosest = notEvaluated.get(0);
+                int index = 0;
+                for (int ind = 1; ind < notEvaluated.size(); ind++) {
+                    TrailPoint tpNotEval = notEvaluated.get(ind);
+                    if (tpNotEval.getDistance() >= tpClosest.getDistance()) {
+                        continue;
+                    }
+                    tpClosest = tpNotEval;
+                    index = ind;
+                }
+                notEvaluated.remove(index);
+            }
+
+            // if we're at end - we're done!
+            if (tpClosest.getMapTile() == end) {
+                tpClosest.getNeighbors().add(tpOn);
+                nodes.put(tpClosest.getMapTile(), tpClosest);
+                break;
+            }
+
+            // try this one
+            tpOn = tpClosest;
+        }
+
+        // Create the return path - from end back to beginning.
+        java.util.ArrayList<Point> path = new java.util.ArrayList<Point>();
+        tpOn = nodes.get(end);
+        path.add(tpOn.getMapTile());
+        while (tpOn.getMapTile() != start) {
+            java.util.ArrayList<TrailPoint> neighbors = tpOn.getNeighbors();
+            int cost = tpOn.getCost();
+
+            tpOn = tpOn.getNeighbors().get(0);
+            for (int ind = 1; ind < neighbors.size(); ind++) {
+                if (neighbors.get(ind).getCost() < tpOn.getCost()) {
+                    tpOn = neighbors.get(ind);
+                }
+            }
+
+            // we didn't get to the start.
+            if (tpOn.getCost() >= cost) {
+                TRAP.trap();
+                return path;
+            }
+            path.add(0, tpOn.getMapTile());
+        }
+
+        return path;
+    }
+
+    private static class TrailPoint {
+        /**
+         * The Map tile for this point in the trail.
+         */
+        private Point privateMapTile;
+
+        public final Point getMapTile() {
+            return privateMapTile;
+        }
+
+        private void setMapTile(Point value) {
+            privateMapTile = value;
+        }
+
+        /**
+         * The neighboring tiles (up to 4). If 0 then this point has been added as a neighbor but is in the
+         * notEvaluated List because it has not yet been tried.
+         */
+        private java.util.ArrayList<TrailPoint> privateNeighbors;
+
+        public final java.util.ArrayList<TrailPoint> getNeighbors() {
+            return privateNeighbors;
+        }
+
+        private void setNeighbors(java.util.ArrayList<TrailPoint> value) {
+            privateNeighbors = value;
+        }
+
+        /**
+         * Estimate of the distance to the end. Direct line if have no neighbors. Best neighbor.Distance + 1
+         * if have neighbors. This value is bad if it's along a trail that failed.
+         */
+        private int privateDistance;
+
+        public final int getDistance() {
+            return privateDistance;
+        }
+
+        private void setDistance(int value) {
+            privateDistance = value;
+        }
+
+        /**
+         * The number of steps from the start to this tile.
+         */
+        private int privateCost;
+
+        public final int getCost() {
+            return privateCost;
+        }
+
+        public final void setCost(int value) {
+            privateCost = value;
+        }
+
+        public TrailPoint(Point pt, Point end, int cost) {
+            setMapTile(pt);
+            setNeighbors(new java.util.ArrayList<TrailPoint>());
+            setDistance(Math.abs(getMapTile().x - end.x) + Math.abs(getMapTile().y - end.y));
+            setCost(cost);
+        }
+
+        public final void RecalculateDistance(Point mapTileCaller, int remainingSteps) {
+
+            TRAP.trap(getDistance() == 0);
+            // if no neighbors then this is in notEvaluated and so can't recalculate.
+            if (getNeighbors().isEmpty()) {
+                return;
+            }
+
+            int shortestDistance = 666;
+            // if just 1 neighbor, then it's a dead end
+            if (getNeighbors().size() == 1) {
+                shortestDistance = DEAD_END;
+            } else {
+                for (TrailPoint neighborOn : getNeighbors()) {
+                    if (shortestDistance > neighborOn.getDistance())
+                        shortestDistance = neighborOn.getDistance();
+                }
+                // it's 1+ lowest neighbor value (unless a dead end)
+                if (shortestDistance != DEAD_END) {
+                    shortestDistance++;
+                }
+            }
+
+            // no change, no need to recalc neighbors
+            if (shortestDistance == getDistance()) {
+                return;
+            }
+
+            // new value (could be longer or shorter)
+            setDistance(shortestDistance);
+
+            // if gone to far, no more recalculate
+            if (remainingSteps-- < 0) {
+                return;
+            }
+
+            //  Need to tell our neighbors - except the one that called us
+//C# TO JAVA CONVERTER TODO TASK: There is no Java equivalent to LINQ queries:
+
+
+            for (TrailPoint neighborOn : getNeighbors()) {
+                if (neighborOn.getMapTile() != mapTileCaller)
+                    neighborOn.RecalculateDistance(getMapTile(), remainingSteps);
+            }
+
+            // and we re-calc again because that could have changed our neighbor's values
+            for (TrailPoint neighborOn : getNeighbors()) {
+                if (shortestDistance > neighborOn.getDistance())
+                    shortestDistance = neighborOn.getDistance();
+            }
+            // it's 1+ lowest neighbor value (unless a dead end)
+            if (shortestDistance != DEAD_END) {
+                shortestDistance++;
+            }
+            setDistance(shortestDistance);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Map=%1$s, Cost=%2$s, Distance=%3$s, Neighbors=%4$s", getMapTile(), getCost(), getDistance(), getNeighbors().size());
+        }
+    }
+}
